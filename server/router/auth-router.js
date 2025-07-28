b80ebba6c918aa183d02eb0a5a12b65a20bda53e
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-07-28 21:44:59 +0530

    routes

---
+++ server/router/auth-router.js
@@ -0,0 +1,10 @@
+
+
+
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("Hello, World!");
+// });
+// app.get("/register", (req, res) => {
+//   res.status(200).send("Registration page");
+// });
