190a46f27d8c409e88eaa53fc5d05d42e6671924
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-07-28 21:52:42 +0530

    route

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,10 +1,15 @@
+const express = require("express");
+// const authRouter = require("./router/auth-router");
+const router = express.Router();
 
 
+router.get("/", (req, res) => {
+  res.status(200).send("Hello, Router!");
+});
 
 
-// app.get("/", (req, res) => {
-//   res.status(200).send("Hello, World!");
-// });
 // app.get("/register", (req, res) => {
 //   res.status(200).send("Registration page");
 // });
+
+module.exports = router;
