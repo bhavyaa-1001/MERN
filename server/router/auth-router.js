d9e404f85892ab2345dec3acc08827bf64ab231c
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-07-29 01:13:50 +0530

    minor changes

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,30 +1,10 @@
 const express = require("express");
-// const authRouter = require("./router/auth-router");
 const router = express.Router();
 const authcontrollers = require("../controllers/auth-controller");
-// router.get("/", (req, res) => {
-//   res
-//         .status(200)
-//         .send("Hello, Router!");
-// });
 
-
-// router.route("/").get((req, res) => {
-
-//   res
-//         .status(200) 
-//         .send("Hello, Router!");
-// });
 
 router.route("/").get(authcontrollers.home);
 router.route("/register").post(authcontrollers.register);
-// router.route("/register").get((req, res) => {
-//   res
-//         .status(200)
-//         .send("Registration page");
-// }); 
-// app.get("/register", (req, res) => {
-//   res.status(200).send("Registration page");
-// });
+
 
 module.exports = router;
