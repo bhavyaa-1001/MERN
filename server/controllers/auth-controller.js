ebb11738dae634813c28a1b6238f903aa48714cd
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-07-29 16:51:37 +0530

    dataTesting

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user-model');
+
 const home = async (req,res) => {
   try{
     res
@@ -13,9 +15,23 @@ const home = async (req,res) => {
 const register = async (req, res) => {
   try {
     console.log(req.body);
-  res
-        .status(200)
-        .json({ message: req.body });
+    const {username, email, password , phone} = req.body;
+
+    const userExist = await User.findOne({email});
+
+    if(userExist){
+      return res.status(400).json({message: "User already exists"});
+    }
+
+   
+    const userCreated = await User.create({
+      username,
+      email,
+      password,
+      phone
+    });
+
+  res.status(200).json({ message: "User registered successfully", user: userCreated });
     } catch (err) {
         res.status(500).json("Internal Server Error");
         console.log(err);
