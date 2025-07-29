1c404651b42e575b5ae58f48e19a31e1e2664840
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-07-29 22:56:39 +0530

    bcryptjs0

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const router = require('./router/auth-router');
+  require('dotenv').config();
+  const express = require('express');
+  const app = express();
+  const router = require('./router/auth-router');
 const connectDB = require('./utils/db');
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.use("/api/auth", router);
-
 
 const PORT = 3000;
 
