6c3c7f3829c284fa786fe536177475019d23599b
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-07-30 21:28:07 +0530

    JWT

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -42,7 +44,19 @@ userSchema.pre('save',async function(next) {
 
 // json web token
 
-
+userSchema.methods.generateToken = async function() {
+  try {
+      return jwt.sign({
+         userId: this._id.toString(),
+          email: this.email,
+          isAdmin: this.isAdmin 
+      },
+      process.env.JWT_SECRET, { expiresIn: '1h' });
+  } catch (error) {
+    console.error(error);
+    
+  }
+};
 
 
 const User = new mongoose.model('User', userSchema);
